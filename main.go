b9b84f777412879d63a0486922196fd0637aeaf5
spiderasasen <spiderasasen@gmail.com>
2026-04-17 08:08:03 -0400

    made the basic adding items to the list

--- main.go
+++ main.go
@@ -5,5 +5,23 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	//makes an emptry array
+	var options []string
+
+	//user makes the choices
+	var choice string
+	for {
+		fmt.Println("Type stop to end.")
+		fmt.Println("Please enter a option: ")
+		fmt.Scan(&choice)
+
+		//if the user types stops, it will end the loop
+		if choice == "stop" || choice == "Stop" {
+			break
+		}
+
+		//otherwise it will appended to the list
+		options = append(options, choice)
+	}
+
 }
